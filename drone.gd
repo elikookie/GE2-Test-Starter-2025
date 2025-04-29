ea2c19620887d2fb914d692ec704803871a086f4
C21320836 Elitsa Koleva <C21320836@mytudublin.ie>
2025-04-29 10:04:30 +0100

    added slowing behaviour for upwards motion

--- drone.gd
+++ drone.gd
@@ -1,7 +1,6 @@
 extends CharacterBody3D
 
-const SPEED = 5.0
-const UP_FORCE = 2.0
+const MAX_SPEED = 3.0
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
@@ -9,16 +8,24 @@ func _physics_process(delta: float) -> void:
 		velocity += get_gravity() * delta
 
 	# Handle flying up.
+	var startSpeed = 0
 	if Input.is_action_pressed("up"):
-		velocity.y = global_position.y + UP_FORCE
+		while startSpeed < MAX_SPEED:
+			startSpeed = startSpeed + 0.1
+			velocity.y = global_position.y + startSpeed
+		velocity.y = global_position.y + startSpeed
+	else:
+		while startSpeed != 0:
+			startSpeed = startSpeed - 0.1
+			velocity.y = global_position.y + startSpeed
 
 	var input_dir := Input.get_vector("left", "right", "forward", "reverse")
 	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	if direction:
-		velocity.x = direction.x * SPEED
-		velocity.z = direction.z * SPEED
+		velocity.x = direction.x * MAX_SPEED
+		velocity.z = direction.z * MAX_SPEED
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-		velocity.z = move_toward(velocity.z, 0, SPEED)
+		velocity.x = move_toward(velocity.x, 0, MAX_SPEED)
+		velocity.z = move_toward(velocity.z, 0, MAX_SPEED)
 
 	move_and_slide()
